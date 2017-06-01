6905afe5b12584e93f4b3c4669290af30c16ac72
Dayana <dayanalara95@gmail.com>
2017-06-01 11:38:24 -0600

    Update Icons

--- archive-events.php
+++ archive-events.php
@@ -1,5 +1,5 @@
 <?php /* Template Name: Events Template */ ?>
- <?php get_header(); ?>
+<?php get_header(); ?>
 
 
 
@@ -7,50 +7,50 @@
 	<div class="row" style="margin: 0 !important;">
 		<div class="col-md-12">
 			<?php
-				$args = array( 'post_type' => 'events', 'post_per_page' => 20 );
-				$loop = new WP_Query( $args );
-				while ( $loop->have_posts() ) : $loop->the_post();
-				 ?>
+			$args = array( 'post_type' => 'events', 'post_per_page' => 20 );
+			$loop = new WP_Query( $args );
+			while ( $loop->have_posts() ) : $loop->the_post();
+			?>
 
-				 	<div class="row" style=" margin: 50px 0;">
-				 		<?php if ( has_post_thumbnail() ) : ?>
-				 		<div class="col-md-4">
-				 			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute();?>">
-				 				<div class="eventsThumbnail" style="background: url('<?php the_post_thumbnail_url(); ?>')"></div>
-				 			</a>
-				 		</div>
-					 <?php endif; ?>
+			<div class="row" style=" margin: 50px 0;">
+				<?php if ( has_post_thumbnail() ) : ?>
+					<div class="col-md-4">
+						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute();?>">
+							<div class="eventsThumbnail" style="background: url('<?php the_post_thumbnail_url(); ?>')"></div>
+						</a>
+					</div>
+				<?php endif; ?>
 
-					 	<div class="col-md-8">
-					 		<h3><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute();?>">
-					 			 <?php the_title_attribute(); ?>
-					 		</a></h3>
+				<div class="col-md-8">
+					<h3><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute();?>">
+						<?php the_title_attribute(); ?>
+					</a></h3>
 
-					 		 <div>
-	                              <p><?php the_excerpt(__('(more?^??)')); ?></p>
-	                          </div>
+					<div>
+						<p><?php the_excerpt(__('(more?^??)')); ?></p>
+					</div>
 
-	                                <a class="read-more" href="<?php  the_permalink() ?>">Read More <i class="fa fa-play" aria-hidden="true"></i></a>
-					 	</div>
+					<a class="read-more" href="<?php  the_permalink() ?>">Read More <i class="fa fa-play" aria-hidden="true"></i></a>
+				</div>
 
-				 	</div>
-                                    
-                                 
-
-
+			</div>
+			
+			
 
 
-                                    <?php
-				endwhile;
+
+
+			<?php
+			endwhile;
 
 
 			?>
-                                   
+			
 		</div>
 		
 	</div>
 </div>
-	
 
 
- <?php get_footer(); ?>
+
+<?php get_footer(); ?>
